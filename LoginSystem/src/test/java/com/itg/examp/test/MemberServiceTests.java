cd1bb07230c67424cda41c7c0acf8f2cf4b97665
itg <itg@DESKTOP-R6AR5V2>
2025-01-21 14:54:17 +0900

    250121

--- LoginSystem/src/test/java/com/itg/examp/test/MemberServiceTests.java
+++ LoginSystem/src/test/java/com/itg/examp/test/MemberServiceTests.java
@@ -11,7 +11,7 @@ import com.itg.examp.MemberService;
 
 @SpringBootTest
 public class MemberServiceTests {
-	//경계값분석: boundary analyze
+	//경계값분석 : boundary analyze
 	String[] rightId;
 	String[] wrongId;
 	String[] rightPw;
@@ -20,21 +20,21 @@ public class MemberServiceTests {
 	MemberService ms;
 	@BeforeEach
 	public void setVariable() {
-		rightId = new String[] {"aa", "abcde", "abcd", "a@E3ool", "bb#cc"};
+		rightId = new String[] {"aa","abcde","abcd","a@E3ool","bb#cc"};
 		wrongId = new String[] {"aaa","abc","aa","a#c","123"};
-		rightPw = new String[] {"123456","214#aa","aa#11aa","a@eegd","443252"};
-		wrongPw = new String[] {"11236","213#a","#11aa","a@gd","AA@EE"};
+		rightPw = new String[] {"123456","214#aa","aa#11aa","a@eegd","44AA@EE"};
+		wrongPw = new String[] {"1123677","214#a","#11aa","a@gd","AA@EE"};
 	}
 	@Test
 	public void testPassword() {
 		boolean res = false;
-		for(int i = 0; i<rightId.length;i++) {
+		for(int i=0;i<rightId.length;i++) {
 			res = ms.checkId(rightId[i]);
-			assertEquals(true, res, ": 올바른 아이디 입니다. :");
+			assertEquals(true, res,i+": 올바른 아이디 입니다. :");
 		}
-		for(int i = 0; i<wrongId.length;i++) {
+		for(int i=0;i<wrongId.length;i++) {
 			res = ms.checkId(wrongId[i]);
-			assertEquals(false,	res,": 잘못된 아이디 입니다. :");
+			assertEquals(false,res,i+": 잘못된 아이디 입니다. :");
 		}
 	}
 	@Test
@@ -42,11 +42,11 @@ public class MemberServiceTests {
 		boolean res = false;
 		for(String pw : rightPw) {
 			res = ms.checkPassword(pw);
-			assertEquals(true, res, ": 올바른 비밀번호 입니다. :");
+			assertEquals(true, res,": 올바른 비밀번호 입니다. :");
 		}
-		for(String pw: wrongPw) {
+		for(String pw:wrongPw) {
 			res = ms.checkPassword(pw);
-			assertEquals(false,	res,": 잘못된 비밀번호 입니다. :");
+			assertEquals(false,res,": 잘못된 비밀번호 입니다. :");
 		}
 	}
 }
